073a6b9e106a0ff5738802c71b9fe6e39abeaee1
jchapar <chaparrojcjr@gmail.com>
2023-02-19 23:47:55 -0700

    mobile nav styles with overlay, open and close functionality added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 const navbar = document.querySelector('nav')
-const openMenu = document.getElementById('menu-button')
-const closeMenu = document.getElementById('menu-close')
+const overlay = document.querySelector('.overlay')
+const mobileNav = document.querySelector('.mobile-nav')
+const openBtn = document.getElementById('open-menu')
+const closeBtn = document.getElementById('close-menu')
 
-openMenu.addEventListener('click', () => {
-  navbar.classList.add('open')
+openBtn.addEventListener('click', () => {
+  overlay.classList.add('active')
+  mobileNav.classList.add('active')
+  openBtn.style.display = 'none'
+  closeBtn.style.display = 'flex'
 })
 
-closeMenu.addEventListener('click', () => {
-  navbar.classList.remove('open')
+closeBtn.addEventListener('click', () => {
+  overlay.classList.remove('active')
+  mobileNav.classList.remove('active')
+  openBtn.style.display = 'flex'
+  closeBtn.style.display = 'none'
 })
